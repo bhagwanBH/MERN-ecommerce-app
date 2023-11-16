aef731e09d6cc0bee3ae26c89d8ddd1e72433a46
bhagwanBH <120296966+bhagwanBH@users.noreply.github.com>
2023-11-16 22:00:23 +0530

    new

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLTopath} from 'url';
 
 //config env
 dotenv.config();
 
 //database config
 connectDB();
-
+//esmodulefix
+const __filename = fileURLTopath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 //rest object
