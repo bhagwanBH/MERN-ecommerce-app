7176c70a6144eb6b5bb74f9ff1f620a04d7dd212
bhagwanBH <120296966+bhagwanBH@users.noreply.github.com>
2023-11-17 09:15:42 +0530

    Update server.jsss

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -36,8 +36,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 //PORT
